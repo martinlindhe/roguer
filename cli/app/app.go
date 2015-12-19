e3999f3c108262c70aeb1abe7b278544b99e32ea
Martin Lindhe <martin@ubique.se>
2015-12-19 22:43:21 +0100

    use gxui

--- cli/app/app.go
+++ cli/app/app.go
@@ -2,87 +2,103 @@ package main
 
 import (
 	"fmt"
-	"image/png"
-	"os"
 
+	"github.com/google/gxui"
+	"github.com/google/gxui/drivers/gl"
+	"github.com/google/gxui/gxfont"
+	"github.com/google/gxui/samples/flags"
 	"github.com/martinlindhe/rogue"
-	"github.com/veandco/go-sdl2/sdl"
-	"github.com/veandco/go-sdl2/sdl_image"
 )
 
-func newWindow() (*sdl.Window, *sdl.Renderer) {
-	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		800, 600, sdl.WINDOW_SHOWN)
-	if err != nil {
-		panic(err)
-	}
-
-	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to create renderer: %s\n", err)
-		panic(err)
-	}
-
-	return window, renderer
+func main() {
+	gl.StartDriver(appMain)
 }
 
-func main() {
+func appMain(driver gxui.Driver) {
 
 	//seed := time.Now().Unix()
-
 	seed := int64(1450549167)
-
 	fmt.Printf("Using seed %d\n", seed)
 
 	island := rogue.GenerateIsland(seed, 220, 100)
-	// -------
 
-	// debug output:
-	islandImage := island.HeightMapAsImage()
-	islandColImage := island.ColoredHeightMapAsImage()
+	//	islandColImage := island.ColoredHeightMapAsImage()
+	/*
+		islandColImgFile, _ := os.Create("island_col.png")
+		png.Encode(islandColImgFile, islandColImage)
 
-	islandImgFile, _ := os.Create("island.png")
-	png.Encode(islandImgFile, islandImage)
+		islandImage := island.HeightMapAsImage()
+		islandImgFile, _ := os.Create("island.png")
+		png.Encode(islandImgFile, islandImage)
+	*/
 
-	islandColImgFile, _ := os.Create("island_col.png")
-	png.Encode(islandColImgFile, islandColImage)
-
-	sdl.Init(sdl.INIT_EVERYTHING)
-
-	window, renderer := newWindow()
-
-	defer window.Destroy()
-	defer renderer.Destroy()
-
-	// XXX how to load a sdl.Image from my rgb buffer, rather than from disk?
-	image, err := img.Load("island_col.png")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to load PNG: %s\n", err)
-		panic(err)
-	}
-	defer image.Free()
-
-	texture, err := renderer.CreateTextureFromSurface(image)
-	if err != nil {
-		panic(err)
-	}
-	defer texture.Destroy()
-
-	srcRect := sdl.Rect{0, 0, int32(island.Width), int32(island.Height)}
-	dstRect := sdl.Rect{0, 0, int32(island.Width), int32(island.Height)}
-
-	renderer.Clear()
-	renderer.Copy(texture, &srcRect, &dstRect)
-	renderer.Present()
-
-	sdl.Delay(1000 * 2)
-
-	// -------
 	island.FillWithCritters()
 	for i := 0; i < 10; i++ {
 		island.Tick()
 	}
 
-	// XXX could this be deferred instead?
-	sdl.Quit()
+	theme := flags.CreateTheme(driver)
+
+	//	font := createMonospaceFont(25, &driver)
+
+	window := theme.CreateWindow(800, 600, "rogue")
+	window.SetBackgroundBrush(gxui.CreateBrush(gxui.Gray50))
+	window.SetScale(flags.DefaultScaleFactor)
+
+	/*
+		img := theme.CreateImage()
+		texture := driver.CreateTexture(islandColImage, 1)
+		img.SetTexture(texture)
+		window.AddChild(img)
+
+		// XXX position!?
+		label := theme.CreateLabel()
+		label.SetFont(font)
+		label.SetText("Hello world")
+		window.AddChild(label)
+	*/
+
+	splitterAB := theme.CreateSplitterLayout()
+	splitterAB.SetOrientation(gxui.Horizontal)
+	splitterAB.AddChild(panelHolder("A", theme))
+	splitterAB.AddChild(panelHolder("B", theme))
+
+	splitterCD := theme.CreateSplitterLayout()
+	splitterCD.SetOrientation(gxui.Horizontal)
+	splitterCD.AddChild(panelHolder("C", theme))
+	splitterCD.AddChild(panelHolder("D", theme))
+
+	vSplitter := theme.CreateSplitterLayout()
+	vSplitter.SetOrientation(gxui.Vertical)
+	vSplitter.AddChild(splitterAB)
+	vSplitter.AddChild(splitterCD)
+	window.AddChild(vSplitter)
+
+	window.OnClose(driver.Terminate)
+}
+
+func createMonospaceFont(size int, driver *gxui.Driver) gxui.Font {
+
+	p := *driver
+
+	font, err := p.CreateFont(gxfont.Monospace, size)
+	if err != nil {
+		panic(err)
+	}
+	return font
+}
+
+// Create a PanelHolder with a 3 panels
+func panelHolder(name string, theme gxui.Theme) gxui.PanelHolder {
+	label := func(text string) gxui.Label {
+		label := theme.CreateLabel()
+		label.SetText(text)
+		return label
+	}
+
+	holder := theme.CreatePanelHolder()
+	holder.AddPanel(label(name+" 0 content"), name+" 0 panel")
+	holder.AddPanel(label(name+" 1 content"), name+" 1 panel")
+	holder.AddPanel(label(name+" 2 content"), name+" 2 panel")
+	return holder
 }
