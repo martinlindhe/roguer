217bc74c0bf4fb2a5345f7cfee5000b96120401a
Martin Lindhe <martin@ubique.se>
2015-12-19 23:06:53 +0100

    gxui wip

--- cli/app/app.go
+++ cli/app/app.go
@@ -20,7 +20,7 @@ func appMain(driver gxui.Driver) {
 	seed := int64(1450549167)
 	fmt.Printf("Using seed %d\n", seed)
 
-	island := rogue.GenerateIsland(seed, 220, 100)
+	island := rogue.GenerateIsland(seed, 220, 140)
 
 	/*
 		islandColImgFile, _ := os.Create("island_col.png")
@@ -38,8 +38,6 @@ func appMain(driver gxui.Driver) {
 
 	theme := flags.CreateTheme(driver)
 
-	//	font := createMonospaceFont(25, &driver)
-
 	window := theme.CreateWindow(800, 600, "rogue")
 	window.SetBackgroundBrush(gxui.CreateBrush(gxui.Gray50))
 	window.SetScale(flags.DefaultScaleFactor)
@@ -53,15 +51,17 @@ func appMain(driver gxui.Driver) {
 		window.AddChild(label)
 	*/
 
+	font, _ := driver.CreateFont(gxfont.Monospace, 25)
+
 	splitterAB := theme.CreateSplitterLayout()
 	splitterAB.SetOrientation(gxui.Horizontal)
 	splitterAB.AddChild(topLeftPanelHolder(&theme, &driver, &window, &island))
-	splitterAB.AddChild(panelHolder("B", theme))
+	splitterAB.AddChild(panelHolder("B", &theme, &font))
 
 	splitterCD := theme.CreateSplitterLayout()
 	splitterCD.SetOrientation(gxui.Horizontal)
-	splitterCD.AddChild(panelHolder("C", theme))
-	splitterCD.AddChild(panelHolder("D", theme))
+	splitterCD.AddChild(panelHolder("C", &theme, &font))
+	splitterCD.AddChild(panelHolder("D", &theme, &font))
 
 	vSplitter := theme.CreateSplitterLayout()
 	vSplitter.SetOrientation(gxui.Vertical)
@@ -72,23 +72,14 @@ func appMain(driver gxui.Driver) {
 	window.OnClose(driver.Terminate)
 }
 
-func createMonospaceFont(size int, driver *gxui.Driver) gxui.Font {
-
-	p := *driver
-
-	font, err := p.CreateFont(gxfont.Monospace, size)
-	if err != nil {
-		panic(err)
-	}
-	return font
-}
-
 // Create a PanelHolder with a 3 panels
 func topLeftPanelHolder(theme *gxui.Theme, driver *gxui.Driver, window *gxui.Window, island *rogue.Island) gxui.PanelHolder {
 
 	name := "top left"
 	label := func(text string) gxui.Label {
 		label := (*theme).CreateLabel()
+		font, _ := (*driver).CreateFont(gxfont.Monospace, 25)
+		label.SetFont(font)
 		label.SetText(text)
 		return label
 	}
@@ -97,23 +88,24 @@ func topLeftPanelHolder(theme *gxui.Theme, driver *gxui.Driver, window *gxui.Win
 	img := (*theme).CreateImage()
 	texture := (*driver).CreateTexture(islandColImage, 1)
 	img.SetTexture(texture)
-	(*window).AddChild(img)
+	//(*window).AddChild(img)
 
 	holder := (*theme).CreatePanelHolder()
-	holder.AddPanel(label(name+" 0 content"), name+" 0 panel")
+	holder.AddPanel(img, "map")
 	holder.AddPanel(label(name+" 1 content"), name+" 1 panel")
 	return holder
 }
 
 // Create a PanelHolder with a 3 panels
-func panelHolder(name string, theme gxui.Theme) gxui.PanelHolder {
+func panelHolder(name string, theme *gxui.Theme, font *gxui.Font) gxui.PanelHolder {
 	label := func(text string) gxui.Label {
-		label := theme.CreateLabel()
+		label := (*theme).CreateLabel()
+		label.SetFont(*font)
 		label.SetText(text)
 		return label
 	}
 
-	holder := theme.CreatePanelHolder()
+	holder := (*theme).CreatePanelHolder()
 	holder.AddPanel(label(name+" 0 content"), name+" 0 panel")
 	holder.AddPanel(label(name+" 1 content"), name+" 1 panel")
 	holder.AddPanel(label(name+" 2 content"), name+" 2 panel")
