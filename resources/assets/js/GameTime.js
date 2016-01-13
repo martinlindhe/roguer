cdf5cf0ebf7ede0f76535c7b3dea4062b3c284f1
Martin Lindhe <martin@ubique.se>
2016-01-13 12:34:56 +0100

    GameTime.js: make test pass

--- resources/assets/js/GameTime.js
+++ resources/assets/js/GameTime.js
@@ -1,3 +1,11 @@
+// time constants
+const Minute = 1;
+const Hour   = Minute * 60;
+const Day    = Hour * 24;
+const Month  = Day * 30;
+const Season = Month * 3;
+const Year   = Month * 12;
+
 export default class GameTime
 {
     constructor(i)
@@ -10,11 +18,51 @@ export default class GameTime
         this.time = i;
     }
 
+    /**
+     * @return string "14:30"
+     */
     render()
     {
-        // XXXX convert int to HH:MM in js
+        var parts = this.dateParts();
 
+        return ("00" + parts.Hour).slice(-2) + ":" + ("00" + parts.Minute).slice(-2);
+    }
 
-        return "hehe " + this.time;
+    dateParts()
+    {
+        var rest = this.time;
+
+        var year = rest / Year;
+        if (year > 0) {
+            rest -= year * Year;
+        }
+
+        var month = rest / Month;
+        if (month > 0) {
+            rest -= month * Month;
+        }
+
+        var day = rest / Day;
+        if (day > 0) {
+            rest -= day * Day;
+        }
+
+        // NOTE: day is stored 0-based, but displayed 1-based
+        day++;
+
+        var hour = rest / Hour;
+        if (hour > 0) {
+            rest -= hour * Hour;
+        }
+
+        var minute = rest;
+
+        return {
+            Minute: minute,
+            Hour: hour,
+            Day: day,
+            Month: month,
+            Year: year
+        };
     }
 }
